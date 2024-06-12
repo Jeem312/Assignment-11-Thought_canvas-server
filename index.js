bd21cc797e2a3ea62cfd7dfe2640995da0e97d69
Jeem312 <Shajidajeem312@gmail.com>
2024-06-12 13:51:25 +0600

    added some api

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:[
+      'http://localhost:5173',
+      'https://assignment-12-f1692.web.app',
+      'https://assignment-12-f1692.web.app'
+      
+
+    ],
+  
+  }
+));
 app.use(express.json());
 
 
@@ -74,12 +85,12 @@ app.use(express.json());
     
       res.send(result);
      })
-     app.get('/users',async(req,res)=>{
+     app.get('/users',verifyToken,async(req,res)=>{
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
-    app.patch('/updateProfile/:email',verifyToken, async(req,res)=>{
+    app.patch('/updateProfile/:email', async(req,res)=>{
       const email = req.params.email;
       
       const filter = {email: email};
@@ -88,7 +99,7 @@ app.use(express.json());
       const data ={
         $set:{
          Name:updatedInfo.name,
-         Company_logo:updatedInfo.Company_logo,
+        //  Company_logo:updatedInfo.Company_logo,
        
         
         }
@@ -96,7 +107,7 @@ app.use(express.json());
         const result = await userCollection.updateOne(filter,data);
          res.send(result);
     })
-    app.patch('/users/:email',verifyToken, async(req,res)=>{
+    app.patch('/users/:email', async(req,res)=>{
       const email = req.params.email;
       
       const filter = {email: email};
@@ -181,7 +192,7 @@ app.delete('/requestedAsset/:id',verifyToken, async (req, res) => {
   res.send(result);
 })
 
-app.patch('/updateAsset/:id',verifyToken, async(req,res)=>{
+app.patch('/updateAsset/:id', async(req,res)=>{
   const id = req.params.id;
  
   const filter = {_id: new ObjectId(id)};
@@ -357,8 +368,8 @@ app.get('/review',async(req,res)=>{
       // Connect the client to the server	(optional starting in v4.7)
     //   await client.connect();
       // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      // await client.db("admin").command({ ping: 1 });
+      // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
       // Ensures that the client will close when you finish/error
     //   await client.close();
